7ba9d195348e68afbea3473de3641920e060fef2
jgam <jgam@alumni.nd.edu>
2020-12-27 00:56:59 +0900

    done

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,24 +1,44 @@
 <template>
-  <div>
-    <app-header v-on:renew="onRenew" v-bind:header="str"></app-header>
-    {{str}}
-  </div>
+  <form v-on:submit.prevent="onSubmit">
+    <div>
+      <label for="username">id : </label>
+      <input type="text" id="username" v-model="username">
+    </div>
+    <div>
+      <label for="password">password : </label>
+      <input type="password" id="password" v-model="password">
+    </div>
+    <button type="submit" >login Button</button>
+  </form>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader';
+import axios from 'axios';
+
 export default {
   data: function(){
     return {
-      str: 'Headerrr'
+      username: '',
+      password: ''
     }
   },
-  components:{
-    'app-header':AppHeader
-  },
   methods:{
-    onRenew: function(){
-      this.str = 'hi'
+    onChange: function(){
+      console.log('onChange clicked');
+    },
+    onSubmit: function(){
+      console.log('submit clicked');
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url,data).then(function(response){
+        console.log(response);
+      }).catch(function(error){
+        console.log(error)
+      });
     }
   }
 }
