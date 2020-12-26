497d06c6f275fd4c4117bea97ef287cf30f38e1f
jgam <jgam@alumni.nd.edu>
2020-12-27 00:27:07 +0900

    vue cli exploration

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <app-header v-on:renew="onRenew" v-bind:header="str"></app-header>
+    {{str}}
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader';
+export default {
+  data: function(){
+    return {
+      str: 'Headerrr'
+    }
+  },
+  components:{
+    'app-header':AppHeader
+  },
+  methods:{
+    onRenew: function(){
+      this.str = 'hi'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <header>
+      <h1>{{header}}</h1>
+      <button v-on:click="onClick"></button>
+  </header>
+</template>
+
+<script>
+export default {
+    methods:{
+        onClick: function(){
+            console.log('clicked')
+            this.$emit('renew');
+        }
+    },
+    props:{
+        header: String
+    }
+}
+</script>
+
+<style>
+
+</style>
